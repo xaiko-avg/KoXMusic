c315b787e928954b88323733c263af1b3bc9e98e
ITS-LUCKY-BOY <152284180+ITS-LUCKY-BOY@users.noreply.github.com>
2023-12-01 15:53:14 +0530

    Update globalban.py

--- VipX/plugins/globalban.py
+++ VipX/plugins/globalban.py
@@ -111,7 +111,7 @@ async def gungabn(client, message: Message, _):
         except Exception:
             pass
     await remove_banned_user(user_id)
-    await message.reply_sticker("CAACAgUAAxkBAAIjbGKPb3oOFXIT3KSxlIoefG7jTLOiAAJuBgAC_415VDBZlDYZrGxCJAQ")
+    await message.reply_sticker("CAACAgUAAx0CdpiMHgACKY5laXZrIf0YNr4gfH7x3D8CbVAYHgACYwoAAmZdSFe1zEHUcc7H6x4E")
     await message.reply_text(
         _["gban_9"].format(mention, number_of_chats)
     )
